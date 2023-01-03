fb54820ffa1551e81d683621877fa54f0a6570a8
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-01-04 02:17:47 +0530

    Created DbContext configure object

---
+++ Src/FoodDesire.DAL/Configure.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FoodDesire.DAL;
+public static class Configure {
+    public static void ConfigureServices(
+        IServiceCollection services, string dbConnectionString
+        ) {
+        services.AddDbContext<FoodDesireContext>(
+            options => options.UseSqlServer(dbConnectionString)
+            );
+    }
+}
