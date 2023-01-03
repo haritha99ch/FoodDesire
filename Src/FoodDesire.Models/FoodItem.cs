4e7f4389edcc4332f1a64f27454d379688321ed6
Haritha Chamara Rathnayake <Haritha Rathnayake>
2023-01-04 01:55:08 +0530

    Created Json entity for FoodItem

--- Src/FoodDesire.Models/FoodItem.cs
+++ Src/FoodDesire.Models/FoodItem.cs
@@ -3,23 +3,12 @@ public sealed class FoodItem: TrackedEntity {
     [Required, NotNull]
     public int RecipeId { get; set; }
     [Required, NotNull]
-    public int OrderId { get; set; }
-    [Required, NotNull]
-    public ICollection<RecipeIngredient>? Ingredients { get; set; } ////TODO: Json column
+    public ICollection<FoodItemIngredient>? FoodItemIngredients { get; set; } ////TODO: Json column
     public int ChefId { get; set; } //Prepares by
 
 
-    private Recipe? _recipe { get; set; }
     [ForeignKey(nameof(RecipeId))]
-    public Recipe? Recipe {
-        get { return _recipe; }
-        set {
-            _recipe = value;
-            Ingredients = value!.RecipeIngredients;
-        }
-    }
-    [ForeignKey(nameof(OrderId))]
-    public Order? Order { get; set; }
+    public Recipe? Recipe { get; set; }
     [ForeignKey(nameof(ChefId))]
     public Chef? Chef { get; set; }
 }
