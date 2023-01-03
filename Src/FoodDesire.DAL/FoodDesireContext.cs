bb6b335f5fd5d8dc8fc35cf3dd639327d9b24671
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-01-04 02:17:46 +0530

    Created initial DbContext

---
+++ Src/FoodDesire.DAL/FoodDesireContext.cs
@@ -0,0 +1,27 @@
+﻿using FoodDesire.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodDesire.DAL;
+public class FoodDesireContext: DbContext {
+    public DbSet<User> User { get; set; }
+    public DbSet<Account> Account { get; set; }
+    public DbSet<Address> Address { get; set; }
+    public DbSet<Admin> Admin { get; set; }
+    public DbSet<Customer> Customer { get; set; }
+    public DbSet<Employee> Employee { get; set; }
+    public DbSet<Deliverer> Deliverer { get; set; }
+    public DbSet<Chef> Chef { get; set; }
+    public DbSet<Supplier> Supplier { get; set; }
+    public DbSet<IngredientCategory> IngredientCategory { get; set; }
+    public DbSet<Ingredient> Ingredient { get; set; }
+    public DbSet<FoodCategory> FoodCategory { get; set; }
+    public DbSet<Recipe> Recipe { get; set; }
+    public DbSet<RecipeIngredient> RecipeIngredient { get; set; }
+    public DbSet<FoodItem> FoodItem { get; set; }
+    public DbSet<Image> Image { get; set; }
+    public DbSet<Order> Order { get; set; }
+    public DbSet<Payment> Payment { get; set; }
+    public DbSet<Delivery> Delivery { get; set; }
+    public DbSet<Supply> Supply { get; set; }
+    public FoodDesireContext(DbContextOptions<FoodDesireContext> options) : base(options) { }
+}
