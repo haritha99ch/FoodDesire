ac60ba2c850a0596eef6f3e85b198580d392d4fa
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-01-04 02:55:05 +0530

    Calculated price setter for FoodItem

--- Src/FoodDesire.Models/FoodItem.cs
+++ Src/FoodDesire.Models/FoodItem.cs
@@ -2,9 +2,20 @@
 public sealed class FoodItem: TrackedEntity {
     [Required, NotNull]
     public int RecipeId { get; set; }
+    private ICollection<FoodItemIngredient>? _foodItemIngredients { get; set; } ////TODO: Json column
     [Required, NotNull]
-    public ICollection<FoodItemIngredient>? FoodItemIngredients { get; set; } ////TODO: Json column
+    public ICollection<FoodItemIngredient>? FoodItemIngredients {
+        get => _foodItemIngredients!;
+        set {
+            value?.ToList().ForEach(fi => {
+                decimal? pricePerMultiplier = Recipe?.RecipeIngredients?
+                    .FirstOrDefault(ri => ri.Id == fi.RecipeIngredientId)?.PricePerMultiplier;
+                Price += Convert.ToDecimal(Convert.ToDouble(pricePerMultiplier) * fi.Multiplier);
+            });
+        }
+    }
     public int ChefId { get; set; } //Prepares by
+    public decimal Price { get; private set; } = decimal.Zero;
 
 
     [ForeignKey(nameof(RecipeId))]
