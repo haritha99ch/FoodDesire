c005833a5fe8c36e5fbe3b594d18b6a6fff26156
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-01-04 20:06:19 +0530

    TEST:New Created a new method GetOne by filtering

--- Src/FoodDesire.DAL/IRepository.cs
+++ Src/FoodDesire.DAL/IRepository.cs
@@ -1,11 +1,9 @@
-﻿using FoodDesire.Models;
-using System.Linq.Expressions;
-
-namespace FoodDesire.DAL;
+﻿namespace FoodDesire.DAL;
 public interface IRepository<T> where T : Entity {
     Task<List<T>> GetAll();
     Task<T> GetByID(int Id);
     Task<List<T>> Get<T2>(Expression<Func<T, bool>> filter, Expression<Func<T, T2>> order); //For filtering
+    Task<T> GetOne(Expression<Func<T, bool>> filter);
     Task<T> Save(T entity);
     Task<T> Add(T entity);
     Task SaveAll(IEnumerable<T> entities);
