9e0a3fd3ce4d687aa47126a078d40d2f79ef5000
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-01-05 20:08:06 +0530

    Updated methods name and intruduce DbSet alias

--- Src/FoodDesire.DAL/Repository.cs
+++ Src/FoodDesire.DAL/Repository.cs
@@ -1,47 +1,49 @@
 ﻿namespace FoodDesire.DAL;
 public class Repository<T>: IRepository<T> where T : Entity {
     protected readonly FoodDesireContext _context;
+    private DbSet<T> entitySet => _context.Set<T>();
+
     public Repository(FoodDesireContext context) {
         _context = context;
     }
 
     public async Task<T> Add(T entity) {
-        EntityEntry<T>? addedEntity = await _context.Set<T>().AddAsync(entity);
+        EntityEntry<T>? addedEntity = await entitySet.AddAsync(entity);
         await _context.SaveChangesAsync();
         return addedEntity.Entity;
     }
 
     public async Task AddAll(IEnumerable<T> entities) {
-        await _context.Set<T>().AddRangeAsync(entities);
+        await entitySet.AddRangeAsync(entities);
         await _context.SaveChangesAsync();
     }
 
     public async Task<bool> Delete(int Id) {
-        EntityEntry<T>? entityDeleted = _context.Set<T>().Remove(await GetByID(Id));
+        EntityEntry<T>? entityDeleted = entitySet.Remove(await GetByID(Id));
         throw new NotImplementedException();
     }
 
     public async Task<List<T>> Get<T2>(Expression<Func<T, bool>> filter, Expression<Func<T, T2>> order) {
-        List<T>? entities = await _context.Set<T>().Where(filter).OrderBy(order).ToListAsync();
+        List<T>? entities = await entitySet.Where(filter).OrderBy(order).ToListAsync();
         return entities;
     }
     public async Task<T> GetOne(Expression<Func<T, bool>> filter) {
-        T? entity = await _context.Set<T>().SingleOrDefaultAsync(filter);
+        T? entity = await entitySet.SingleOrDefaultAsync(filter);
         return entity!;
     }
 
     public async Task<List<T>> GetAll() {
-        List<T>? entities = await _context.Set<T>().AsNoTracking().ToListAsync();
+        List<T>? entities = await entitySet.AsNoTracking().ToListAsync();
         return entities;
     }
 
     public async Task<T> GetByID(int Id) {
-        T? entity = await _context.Set<T>().FindAsync(Id);
+        T? entity = await entitySet.FindAsync(Id);
         return entity!;
     }
 
-    public async Task<T> Save(T entity) {
-        EntityEntry<T>? updatedEntity = _context.Set<T>().Update(entity);
+    public async Task<T> Update(T entity) {
+        EntityEntry<T>? updatedEntity = entitySet.Update(entity);
         await _context.SaveChangesAsync();
         return updatedEntity.Entity;
     }
