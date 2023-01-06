a987b6161f833b74db1ce85066d82fe5a3265f0a
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-01-06 20:31:08 +0530

    Created IUserServices for common CRUD Operations

---
+++ Src/FoodDesire.Core/IUserService.cs
@@ -0,0 +1,9 @@
+﻿namespace FoodDesire.Core;
+public interface IUserService<T> where T : User {
+    Task<T> CreateAccount(T user);
+    Task<T> GetById(int id);
+    Task<List<T>> GetAll();
+    Task<T> GetByEmailAndPassword(string email, string password);
+    Task<T> UpdateAccount(T user);
+    Task<bool> DeleteAccountById(int id);
+}
