db57600bb31031c1c4df9dab7f3fd9985efb20df
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-01-06 20:32:07 +0530

    Created Customer Services

---
+++ Src/FoodDesire.Core/ICustomerService.cs
@@ -0,0 +1,7 @@
+﻿namespace FoodDesire.Core;
+public interface ICustomerService<Customer>: IUserService<Customer> where Customer : User {
+    Task<Order> CreateOrder(Order order);
+    Task<Order> AddFoodItemToOrder(int orderId, FoodItem foodItem);
+    Task<Order> RemoveFoodItemFromOrder(int orderId, int foodItemId);
+    Task<bool> PayForOrder(int orderId);
+}
--- Src/FoodDesire.Core/Usings.cs
+++ Src/FoodDesire.Core/Usings.cs
@@ -1,1 +1,4 @@
-﻿
+﻿global using FoodDesire.DAL;
+global using FoodDesire.Models;
+global using Microsoft.EntityFrameworkCore;
+global using System.Linq.Expressions;
