cd86b9cf4b9f81810eb65e96e1f0e4d3ce4aa9f4
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-01-08 10:26:34 +0530

    Implemented employee salary payment in Payment servic

--- Src/FoodDesire.Core/IPaymentService.cs
+++ Src/FoodDesire.Core/IPaymentService.cs
@@ -1,5 +1,6 @@
 ﻿namespace FoodDesire.Core;
 public interface IPaymentService {
     Task<Payment> PaymentForSupply(Supply supply, decimal value);
+    Task<Payment> SalaryForEmployee(Payment payment);
     Task SavePayment();
 }
