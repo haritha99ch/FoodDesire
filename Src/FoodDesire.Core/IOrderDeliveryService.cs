a98e05bf9b98b6ce51e8a6639335625b948b7d8e
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-01-09 01:19:33 +0530

    Created order delivery services

---
+++ Src/FoodDesire.Core/IOrderDeliveryService.cs
@@ -0,0 +1,6 @@
+﻿namespace FoodDesire.Core;
+public interface IOrderDeliveryService {
+    Task<Delivery> NewDeliveryForOrder(Delivery delivery);
+    Task<Delivery> OrderIsDelivered(int deliveryId);
+    Task<Delivery> GetDeliveryById(int deliveryId);
+}
---
+++ Src/FoodDesire.Core/OrderDeliveryService.cs
@@ -0,0 +1,25 @@
+﻿namespace FoodDesire.Core;
+public class OrderDeliveryService: IOrderDeliveryService {
+    private readonly IRepository<Delivery> _deliveryRepository;
+
+    public OrderDeliveryService(IRepository<Delivery> deliveryRepository) {
+        _deliveryRepository = deliveryRepository;
+    }
+
+    public async Task<Delivery> GetDeliveryById(int deliveryId) {
+        Delivery delivery = await _deliveryRepository.GetByID(deliveryId);
+        return delivery;
+    }
+
+    public async Task<Delivery> NewDeliveryForOrder(Delivery delivery) {
+        Delivery newDelivery = await _deliveryRepository.Add(delivery);
+        return newDelivery;
+    }
+
+    public async Task<Delivery> OrderIsDelivered(int deliveryId) {
+        Delivery delivery = await _deliveryRepository.GetByID(deliveryId);
+        delivery.IsDelivered = true;
+        delivery = await _deliveryRepository.Update(delivery);
+        return delivery;
+    }
+}
