bbcd861a4a7498ce7fa170ae7d585a40816fc0a5
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-01-09 06:16:56 +0530

    Created order services

---
+++ Src/FoodDesire.Core/IOrderService.cs
@@ -0,0 +1,9 @@
+﻿namespace FoodDesire.Core;
+public interface IOrderService {
+    Task<Order> NewOrder(Order order);
+    Task<List<Order>> GetAllOrders();
+    Task<Order> GetOrderById(int orderId);
+    Task<List<Order>> GetAllOrdersToDeliver();
+    Task<List<Order>> GetAllDeliveredOrders();
+    Task<bool> DeleteOrderById(int orderId);
+}
