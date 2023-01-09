d18e102d78b02a5d9e8566e178e3aa6cddfb306a
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-01-09 07:25:45 +0530

    Modyfied food item services

--- Src/FoodDesire.Core/IFoodItemService.cs
+++ Src/FoodDesire.Core/IFoodItemService.cs
@@ -3,7 +3,6 @@ public interface IFoodItemService {
     Task<FoodItem> NewFoodItemForOrder(FoodItem foodItem);
     Task<bool> RemoveFoodItem(int foodItemId);
     Task<FoodItem> UpdateFoodItem(FoodItem foodItem);
-    Task<List<FoodItem>> GetAllFoodItemsForOrder(int orderId);
     Task<FoodItem> PrepareFoodItem(int foodItemId, int chefId);
     Task<bool> FoodItemPrepared(int foodItemId);
 }
