bda1bde5aac6934526c947bc1d6fe4e4fa05eec7
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-01-09 07:14:27 +0530

    Created Customer order services

---
+++ Src/FoodDesire.Core/CustomerOrderService.cs
@@ -0,0 +1,37 @@
+﻿namespace FoodDesire.Core;
+public class CustomerOrderService: ICustomerOrderService {
+    private readonly IRepository<Customer> _customerRepository;
+    private readonly IRepository<Order> _orderRepository;
+
+    public CustomerOrderService(
+        IRepository<Customer> customerRepository,
+        IRepository<Order> orderRepository
+        ) {
+        _customerRepository = customerRepository;
+        _orderRepository = orderRepository;
+    }
+
+    public async Task<List<Order>> GetAllOrdersForCustomerById(int customerId) {
+        Expression<Func<Order, bool>> filter = e => e.CustomerId == customerId;
+        Expression<Func<Order, DateTime>> order = e => e.DateTime;
+
+        List<Order> orders = await _orderRepository.Get(filter, order);
+        return orders;
+    }
+
+    public async Task<List<Order>> GetAllOrdersToDeliverForCustomerById(int customerId) {
+        Expression<Func<Order, bool>> filter = e => e.CustomerId == customerId && e.Status == OrderStatus.Prepared;
+        Expression<Func<Order, DateTime>> order = e => e.DateTime;
+
+        List<Order> orders = await _orderRepository.Get(filter, order);
+        return orders;
+    }
+
+    public async Task<List<Order>> GetAllPendingOrdersForCustomerById(int customerId) {
+        Expression<Func<Order, bool>> filter = e => e.CustomerId == customerId && e.Status == OrderStatus.Pending;
+        Expression<Func<Order, DateTime>> order = e => e.DateTime;
+
+        List<Order> orders = await _orderRepository.Get(filter, order);
+        return orders;
+    }
+}
