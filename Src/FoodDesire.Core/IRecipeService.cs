012558cbe5b24c6f814534a6e4079f0cb5473a94
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-01-09 10:16:34 +0530

    Created recipe services

---
+++ Src/FoodDesire.Core/IRecipeService.cs
@@ -0,0 +1,12 @@
+﻿namespace FoodDesire.Core;
+public interface IRecipeService {
+    Task<Recipe> NewRecipe(Recipe recipe);
+    Task<Recipe> GetRecipeById(int recipeId);
+    Task<List<Recipe>> GetAllRecipe();
+    Task<List<Recipe>> GetAllRecipeByCategory(string category);
+    Task<List<RecipeIngredient>> GetAllRecipeIngredientsOfRecipe(int recipeId);
+    Task<Recipe> UpdateRecipe(Recipe recipe);
+    Task<Recipe> AddRecipeIngredientToRecipe(int recipeId, RecipeIngredient recipeIngredient);
+    Task<bool> RemoveRecipeIngredientById(int recipeIngredientId);
+    Task<bool> RemoveRecipeById(int recipeId);
+}
