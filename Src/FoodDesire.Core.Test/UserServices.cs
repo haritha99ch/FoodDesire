327ec740b385ecf15bd40cccb70347761c7c200e
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-01-10 16:31:30 +0530

    Core:Test:Fix introduced required property of City in Supplier model

--- Src/FoodDesire.Core.Test/UserServices.cs
+++ Src/FoodDesire.Core.Test/UserServices.cs
@@ -4,7 +4,9 @@ namespace FoodDesire.Core.Test;
 [TestFixture]
 public class UserServices {
     private IAdminService adminService;
+    private ISupplierService supplierService;
     private IRepository<Admin> _adminRepository;
+    private IRepository<Supplier> _supplierRepository;
     private ITrackingRepository<User> _userTRepository;
     private protected FoodDesireContext _context;
 
@@ -12,14 +14,20 @@ public class UserServices {
     public void Setup() {
         _context = DbContextHelper.GetContext("UserServices");
         if(!_context.Database.EnsureCreated()) return;
-        _adminRepository = new Repository<Admin>(_context);
         _userTRepository = new TrackingRepository<User>(_context);
+
+        _adminRepository = new Repository<Admin>(_context);
         adminService = new AdminService(_context, _adminRepository, _userTRepository);
+
+        _supplierRepository = new Repository<Supplier>(_context);
+        supplierService = new SupplierService(_context, _supplierRepository, _userTRepository);
+
+
     }
 
     [OneTimeTearDown]
     public void TearDown() {
-        //_context.Database.EnsureDeleted();
+        _context.Database.EnsureDeleted();
     }
     [Test, Order(1)]
     public async Task CreateAnAdmin() {
@@ -53,10 +61,49 @@ public class UserServices {
     [Test, Order(3)]
     public async Task UpdateAdmin() {
         Admin admin = await adminService.GetByIdPopulated(1);
-
         admin.User!.FirstName = "AdminUpdated";
         await adminService.UpdateAccount(admin);
         Admin updateAdmin = await adminService.GetByIdPopulated(1);
         Assert.That(admin.User.FirstName, Is.EqualTo(updateAdmin.User!.FirstName));
+    }
+    [Test, Order(4)]
+    public async Task CreateAnSupplier() {
+        Supplier supplier = new() {
+            Employee = new Employee {
+                User = new User() {
+                    FirstName = "Supplier",
+                    LastName = "Reilppus",
+                    DateOfBirth = new DateTime(1999, 5, 2),
+                    Account = new Account() {
+                        Email = "supplier@fooddesire.com",
+                        Password = "1234",
+                    },
+                    Address = new Address() {
+                        No = "2",
+                        Street1 = "Street1",
+                        Street2 = "Street2",
+                        City = "Diyatalawa",
+                        PostalCode = 1290
+                    },
+                    Gender = Gender.Male,
+                },
+            },
+            City = "Diyatalawa"
+        };
+        Supplier saveSupplier = await supplierService.CreateAccount(supplier);
+        Assert.That(supplier.Employee.User.FirstName, Is.EqualTo(saveSupplier.Employee!.User!.FirstName));
+    }
+    [Test, Order(5)]
+    public async Task LoginAsSupplier() {
+        Supplier supplier = await supplierService.GetByEmailAndPassword("supplier@fooddesire.com", "1234");
+        Assert.IsNotNull(supplier);
+    }
+    [Test, Order(6)]
+    public async Task UpdateSupplier() {
+        Supplier supplier = await supplierService.GetByIdPopulated(1);
+        supplier.Employee!.User!.FirstName = "SupplierUpdated";
+        await supplierService.UpdateAccount(supplier);
+        Supplier updateAdmin = await supplierService.GetByIdPopulated(1);
+        Assert.That(supplier.Employee!.User!.FirstName, Is.EqualTo(updateAdmin.Employee!.User!.FirstName));
     }
 }
--- Src/FoodDesire.Models/Supplier.cs
+++ Src/FoodDesire.Models/Supplier.cs
@@ -3,7 +3,7 @@ public sealed class Supplier: Entity {
     [Required, NotNull]
     public int EmployeeId { get; set; }
     [Required, NotNull]
-    public string? City { get; set; }
+    public required string City { get; set; }
 
     [ForeignKey(nameof(EmployeeId))]
     public Employee? Employee { get; set; }
