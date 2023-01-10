dc31974750014ec0894b7ac13d4cc6a949da3ecd
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-01-10 11:50:46 +0530

    Created configure servics methods

---
+++ Src/FoodDesire.Core/Configure.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FoodDesire.Core;
+public static class Configure {
+
+    public static void ConfigureServicesForIMS(IServiceCollection services) {
+        ConfigureServices(services);
+        services.AddTransient<IAdminService, AdminService>();
+        services.AddTransient<IChefService, IChefService>();
+        services.AddTransient<IDelivererService, DelivererService>();
+        services.AddTransient<ISupplierService, ISupplierService>();
+        services.AddTransient<IIngredientService, IngredientService>();
+        services.AddTransient<IOrderDeliveryService, OrderDeliveryService>();
+    }
+
+    public static void ConfigureServicesForWebAPI(IServiceCollection services) {
+        ConfigureServices(services);
+        services.AddTransient<ICustomerService, CustomerService>();
+        services.AddTransient<ICustomerOrderService, CustomerOrderService>();
+    }
+
+    private static void ConfigureServices(IServiceCollection services) {
+        services.AddTransient<IOrderService, OrderService>();
+        services.AddTransient<IFoodItemService, FoodItemService>();
+        services.AddTransient<IPaymentService, PaymentService>();
+        services.AddTransient<IRecipeService, RecipeService>();
+    }
+}
--- Src/FoodDesire.DAL/Configure.cs
+++ Src/FoodDesire.DAL/Configure.cs
@@ -8,14 +8,20 @@ public static class Configure {
         IServiceCollection services,
         string dbConnectionString
         ) {
-
+        ConfigureServices(services, dbConnectionString);
+        services.AddScoped<IRepository<Admin>, Repository<Admin>>();
+        services.AddScoped<IRepository<Chef>, Repository<Chef>>();
+        services.AddScoped<IRepository<Deliverer>, Repository<Deliverer>>();
+        services.AddScoped<ITrackingRepository<Supply>, ITrackingRepository<Supply>>();
+        services.AddScoped<ITrackingRepository<IngredientCategory>, ITrackingRepository<IngredientCategory>>();
     }
 
     public static void ConfigureServicesForWebAPI(
         IServiceCollection services,
         string dbConnectionString
         ) {
-
+        ConfigureServices(services, dbConnectionString);
+        services.AddScoped<IRepository<Customer>, Repository<Customer>>();
     }
 
     public static void ConfigureServices(
@@ -25,11 +31,15 @@ public static class Configure {
         services.AddDbContext<FoodDesireContext>(
             options =>
                 options.AddInterceptors(IgnoringIdentityResolutionInterceptor).UseSqlServer(dbConnectionString));
-
+        services.AddScoped<IRepository<Ingredient>, Repository<Ingredient>>();
+        services.AddScoped<IRepository<FoodItem>, Repository<FoodItem>>();
+        services.AddScoped<IRepository<Delivery>, Repository<Delivery>>();
+        services.AddScoped<IRepository<Order>, Repository<Order>>();
         services.AddScoped<IRepository<Account>, Repository<Account>>();
-        services.AddScoped<ITrackingRepository<User>, ITrackingRepository<User>>();
-        services.AddScoped<ITrackingRepository<Supply>, ITrackingRepository<Supply>>();
-        services.AddScoped<ITrackingRepository<IngredientCategory>, ITrackingRepository<IngredientCategory>>();
         services.AddScoped<ITrackingRepository<Payment>, ITrackingRepository<Payment>>();
+        services.AddScoped<ITrackingRepository<User>, ITrackingRepository<User>>();
+        services.AddScoped<IRepository<RecipeCategory>, Repository<RecipeCategory>>();
+        services.AddScoped<IRepository<RecipeIngredient>, Repository<RecipeIngredient>>();
+        services.AddScoped<IRepository<Recipe>, Repository<Recipe>>();
     }
 }
