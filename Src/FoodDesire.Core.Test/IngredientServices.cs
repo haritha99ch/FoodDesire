48109a8582eacea2f6a85c2411da126875349693
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-02-04 15:04:31 +0530

    Created Ingredeint service testing

---
+++ Src/FoodDesire.Core.Test/IngredientServices.cs
@@ -0,0 +1,19 @@
+﻿namespace FoodDesire.Core.Test;
+[TestFixture]
+public class IngredientServices {
+    private ISupplierService _supplierService;
+    private IIngredientService _ingredientService;
+
+    public IngredientServices(ISupplierService supplierService, IIngredientService ingredientService) {
+        _supplierService = supplierService;
+        _ingredientService = ingredientService;
+    }
+
+    public async Task NewIngredient() {
+        IngredientCategory? ingredientCategory = await _ingredientService.NewIngredientCategory(new IngredientCategory() {
+            Name = "Key Ingredients",
+            Description = "Key Ingredients",
+        });
+        Assert.That(ingredientCategory.Name, Is.EqualTo("Key Ingredients"));
+    }
+}
