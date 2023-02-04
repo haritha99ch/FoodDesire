901ac1bec71e510dbfb4c1e7cb7a1f56ecf500be
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-02-04 14:59:31 +0530

    Introduced Extentions Hosting for Testing

---
+++ Src/FoodDesire.Core.Test/IngredientServicesHosting.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FoodDesire.Core.Test;
+[TestFixture]
+public class IngredientServicesHosting {
+    private IngredientServices _testRunner;
+    public IngredientServicesHosting() {
+        var hostBuilder = Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) => {
+                services.AddDbContext<FoodDesireContext>(options => {
+                    DbContextHelper.ConfigureDbContextOptions("IngredientServices", options);
+                });
+                DAL.Configure.ConfigureAllForTesting(services);
+                Core.Configure.ConfigureAllForTesting(services);
+                services.AddTransient<IngredientServices>();
+            });
+        var host = hostBuilder.Build();
+        _testRunner = host.Services.GetRequiredService<IngredientServices>();
+    }
+
+
+    [Test, Order(1)]
+    public async Task NewIngredient() {
+        await _testRunner.NewIngredient();
+    }
+}
