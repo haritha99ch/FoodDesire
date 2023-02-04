2cb64afd141a6955a0d2bad06172ac4877e6e014
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-02-04 20:34:47 +0530

    Created Application host helper to simplify DI

---
+++ Src/FoodDesire.Core.Test/Helpers/ApplicationHostHelper.cs
@@ -0,0 +1,27 @@
+﻿namespace FoodDesire.Core.Test.Helpers;
+public static class ApplicationHostHelper {
+    private static IHost? _host;
+    //This helper method will register all the services using Dependency injection
+    public static IHost ConfigureHost(string contextName) {
+        _host = Host.CreateDefaultBuilder()
+            .ConfigureServices((context, services) => {
+                services.AddDbContext<FoodDesireContext>(options => {
+                    DbContextHelper.ConfigureDbContextOptions(contextName, options);
+                });
+                DAL.Configure.ConfigureAllForTesting(services);
+                Core.Configure.ConfigureAllForTesting(services);
+                services.AddTransient<IngredientServices>();
+            }).Build();
+        return _host;
+    }
+
+    public static T GetService<T>() where T : class {
+        if(!(_host!.Services.GetRequiredService(typeof(T)) is not T service)) return service;
+
+        throw new ArgumentException($"{typeof(T)} needs to be registered in ConfigureServices within DAL.Configure or Core.Configure");
+    }
+
+    public static void TearDownHost() {
+        _host!.Dispose();
+    }
+}
