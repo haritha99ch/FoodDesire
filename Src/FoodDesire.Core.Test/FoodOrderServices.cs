d2155c0d2244b3a5344f5428be02804900eb1bc2
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-02-09 00:17:14 +0530

    Created FoodOrdering test file

---
+++ Src/FoodDesire.Core.Test/FoodOrderServices.cs
@@ -0,0 +1,44 @@
+﻿namespace FoodDesire.Core.Test;
+[TestFixture]
+public class FoodOrderServices {
+    private readonly IFoodItemService _foodItemService;
+    private readonly IIngredientService _ingredientService;
+    private readonly IRecipeService _recipeService;
+    private readonly IChefService _chefService;
+    private readonly FoodDesireContext _context;
+    public FoodOrderServices() {
+        ApplicationHostHelper.ConfigureHost("FoodOrderServices");
+
+        _foodItemService = ApplicationHostHelper.GetService<IFoodItemService>();
+        _ingredientService = ApplicationHostHelper.GetService<IIngredientService>();
+        _recipeService = ApplicationHostHelper.GetService<IRecipeService>();
+        _chefService = ApplicationHostHelper.GetService<IChefService>();
+        _context = ApplicationHostHelper.GetService<FoodDesireContext>();
+    }
+
+    [OneTimeSetUp]
+    public async Task Setup() {
+        await _context.Database.EnsureDeletedAsync();
+        await _context.Database.EnsureCreatedAsync();
+
+        await _recipeService.NewRecipeCategory(RecipeDataHelper.GetRecipeCategoryPayload());
+        await _ingredientService.NewIngredientCategory(IngredientDataHelper.GetIngredientCategoryPayload());
+        await _chefService.CreateAccount(UserDataHelper.GetChefPayload());
+        RecipeDataHelper.GetIngredients()
+            .ForEach(async x => {
+                await _ingredientService.NewIngredient(x);
+            });
+        await _recipeService.NewRecipe(RecipeDataHelper.GetRecipe());
+    }
+
+    [OneTimeTearDown]
+    public async Task TearDown() {
+        //await _context.Database.EnsureDeletedAsync();
+        ApplicationHostHelper.TearDownHost();
+    }
+
+    [Test, Order(1)]
+    public async Task NewFoodItem() {
+
+    }
+}
--- Src/FoodDesire.Core.Test/Helpers/RecipeDataHelper.cs
+++ Src/FoodDesire.Core.Test/Helpers/RecipeDataHelper.cs
@@ -30,4 +30,91 @@ internal static class RecipeDataHelper {
             IsRequired = true,
         };
     }
+
+    internal static List<Ingredient> GetIngredients() {
+        return new List<Ingredient>{
+            new Ingredient {
+                Name = "Flour",
+                Description = "All-purpose flour",
+                Measurement = Measurement.Grams,
+                CurrentPricePerUnit = 0.5,
+                MaximumQuantity = 1000,
+                CurrentQuantity = 500,
+                IngredientCategoryId = 1,
+            },
+            new Ingredient {
+                Name = "Sugar",
+                Description = "Granulated white sugar",
+                Measurement = Measurement.Grams,
+                CurrentPricePerUnit = 0.75,
+                MaximumQuantity = 500,
+                CurrentQuantity = 200,
+                IngredientCategoryId = 1,
+            },
+            new Ingredient {
+                Name = "Eggs",
+                Description = "Grade A large eggs",
+                Measurement = Measurement.Each,
+                CurrentPricePerUnit = 0.25,
+                MaximumQuantity = 200,
+                CurrentQuantity = 150,
+                IngredientCategoryId = 1,
+            },
+            new Ingredient {
+                Name = "Butter",
+                Description = "Unsalted sweet cream butter",
+                Measurement = Measurement.Grams,
+                CurrentPricePerUnit = 1.0,
+                MaximumQuantity = 400,
+                CurrentQuantity = 300,
+                IngredientCategoryId = 1,
+            },
+        };
+    }
+
+    internal static Recipe GetRecipe() {
+        return new Recipe {
+            ChefId = 1,
+            Name = "Chocolate Chip Cookies",
+            Description = "Soft and chewy chocolate chip cookies",
+            MinimumPrice = 3.0m,
+            FixedPrice = 3.5m,
+            RecipeCategoryId = 1,
+            Tags = "dessert, sweet, snack",
+            RecipeIngredients = new List<RecipeIngredient>{
+                new RecipeIngredient {
+                    RecipeId = 1,
+                    IngredientId = 1,
+                    Amount = 200,
+                    RecommendedAmount = 200,
+                    IsRequired = true,
+                    PricePerMultiplier = 0.5m
+                },
+                new RecipeIngredient {
+                    RecipeId = 1,
+                    IngredientId = 2,
+                    Amount = 100,
+                    RecommendedAmount = 100,
+                    IsRequired = true,
+                    PricePerMultiplier = 0.75m
+                },
+                new RecipeIngredient {
+                    RecipeId = 1,
+                    IngredientId = 3,
+                    Amount = 2,
+                    RecommendedAmount = 2,
+                    IsRequired = true,
+                    PricePerMultiplier = 0.25m
+                },
+                new RecipeIngredient {
+                    RecipeId = 1,
+                    IngredientId = 4,
+                    Amount = 150,
+                    RecommendedAmount = 150,
+                    IsRequired = true,
+                    PricePerMultiplier = 1.0m
+                }
+            }
+        };
+    }
 }
