7cb382664bcfa5114d8e169a96ff52878c602520
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-02-18 22:26:33 +0530

    Code cleanup

--- Src/FoodDesire.Core.Test/OrderDeliveryServices.cs
+++ Src/FoodDesire.Core.Test/OrderDeliveryServices.cs
@@ -1,7 +1,27 @@
-﻿namespace FoodDesire.Core.Test;
-public class OrderDeliveryServices {
+namespace FoodDesire.Core.Test;
+[TestFixture]
+public class OrderDeliveryServices{
+    private readonly IOrderDeliveryService _orderDeliveryServices;
+    private readonly IOrderService _orderService;
+    private readonly FoodDesireContext _context;
+    public OrderDeliveryServices(){
+        ApplicationHostHelper.ConfigureHost("OrderDeliveryServices");
 
-    public OrderDeliveryServices() {
-
+        _orderDeliveryServices = ApplicationHostHelper.GetService<IOrderDeliveryService>();
+        _orderService = ApplicationHostHelper.GetService<IOrderService>();
+        _context = ApplicationHostHelper.GetService<FoodDesireContext>();
     }
+
+    [OneTimeSetUp]
+    public async Task Setup(){
+        await _context.Database.EnsureDeletedAsync();
+        await _context.Database.EnsureCreatedAsync();
+    }
+
+    [OneTimeTearDown]
+    public async Task TearDown(){
+        await _context.Database.EnsureDeletedAsync();
+        ApplicationHostHelper.TearDownHost();
+    }
+
 }
