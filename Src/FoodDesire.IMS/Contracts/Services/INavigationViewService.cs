50ae063d5278df7f85ac1d504287af899e77271a
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-02-28 18:11:54 +0530

    Created contracts for navigation services

---
+++ Src/FoodDesire.IMS/Contracts/Services/INavigationViewService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System.Collections.Generic;
+
+namespace FoodDesire.IMS.Contracts.Services;
+public interface INavigationViewService {
+    IList<object>? MenuItems { get; }
+    object? SettingsItem { get; }
+
+    void Initialize(NavigationView navigationView);
+    void UnregisterEvents();
+    NavigationViewItem? GetSelectedItem(Type pageType);
+}
---
+++ Src/FoodDesire.IMS/Contracts/Services/IPageService.cs
@@ -0,0 +1,4 @@
+﻿namespace FoodDesire.IMS.Contracts.Services;
+public interface IPageService {
+    Type GetPageType(string pageKey); //To implement the page view model
+}
