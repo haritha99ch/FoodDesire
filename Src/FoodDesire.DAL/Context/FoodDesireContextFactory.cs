a6acea17a3ac76c9ad4a74ea457ce9488c3041b9
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-03-01 20:05:53 +0530

    Allowed to run db migrations

---
+++ Src/FoodDesire.DAL/Context/FoodDesireContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace FoodDesire.DAL.Context;
+public class FoodDesireContextFactory : IDesignTimeDbContextFactory<FoodDesireContext> {
+    public FoodDesireContext CreateDbContext(string[] args) {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.Migrations.json", optional: false, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        string? connectionString = config.GetConnectionString("DefaultConnection");
+        DbContextOptionsBuilder<FoodDesireContext> builder = new DbContextOptionsBuilder<FoodDesireContext>();
+        builder.UseSqlServer(connectionString);
+        return new FoodDesireContext(builder.Options);
+    }
+}
