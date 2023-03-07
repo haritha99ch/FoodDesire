a18e97a62e8db0e8b8e69640dfd6b84012d856b9
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-03-07 17:28:06 +0530

    IMS: Abstracted App.cs configurations in AppConfigurator helper class

--- Src/FoodDesire.IMS/App.xaml.cs
+++ Src/FoodDesire.IMS/App.xaml.cs
@@ -1,7 +1,4 @@
-﻿using FoodDesire.IMS.Activation;
-using FoodDesire.IMS.Services;
-
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -13,51 +10,17 @@ public partial class App : Application {
 
     public App() {
         InitializeComponent();
-
         Host = Microsoft.Extensions.Hosting.Host
             .CreateDefaultBuilder()
             .UseContentRoot(AppContext.BaseDirectory)
-            .ConfigureAppConfiguration((context, config) => {
-                string environmentName = context.HostingEnvironment.EnvironmentName;
-                AppSettings.Configure.ConfigureEnvironment(config, environmentName);
-            })
-            .ConfigureServices((context, services) => {
-                // Default Activation Handler
-                services.AddTransient<ActivationHandler<LaunchActivatedEventArgs>, DefaultActivationHandler>();
-
-                // Other Activation Handlers
-                // Core Services
-                string connectionString = context.Configuration.GetConnectionString("DefaultConnection")!;
-                Core.Configure.ConfigureServices(services, connectionString);
-
-                // Services
-                services.AddSingleton<ILocalSettingsService, LocalSettingsService>();
-                services.AddSingleton<IThemeSelectorService, ThemeSelectorService>();
-                services.AddTransient<INavigationViewService, NavigationViewService>();
-
-                services.AddSingleton<IActivationService, ActivationService>();
-                services.AddSingleton<IPageService, PageService>();
-                services.AddSingleton<INavigationService, NavigationService>();
-
-                // Views and ViewModels
-                services.AddTransient<SettingsViewModel>();
-                services.AddTransient<SettingsPage>();
-                services.AddTransient<HomeViewModel>();
-                services.AddTransient<HomePage>();
-                services.AddTransient<IngredientsViewModel>();
-                services.AddTransient<IngredientsPage>();
-                services.AddTransient<ShellViewModel>();
-                services.AddTransient<ShellPage>();
-
-                // Configuration
-                services.Configure<LocalSettingsOptions>(context.Configuration.GetSection(nameof(LocalSettingsOptions)));
-            }).Build();
+            .ConfigureAppConfiguration(AppConfigurator.Configure)
+            .ConfigureServices(AppConfigurator.Configure)
+            .Build();
     }
 
-    public static T GetService<T>()
-    where T : class {
+    public static T GetService<T>() where T : class {
         if ((Current as App)!.Host.Services.GetService(typeof(T)) is not T service) {
-            throw new ArgumentException($"{typeof(T)} needs to be registered in ConfigureServices within App.xaml.cs.");
+            throw new ArgumentException($"{typeof(T)} needs to be registered in ConfigureServices within AppConfigurator.cs.");
         }
         return service;
     }
---
+++ Src/FoodDesire.IMS/Helpers/AppConfigurator.cs
@@ -0,0 +1,46 @@
+﻿using FoodDesire.IMS.Activation;
+using FoodDesire.IMS.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace FoodDesire.IMS.Helpers;
+internal static class AppConfigurator {
+    internal static void Configure(HostBuilderContext context, IConfigurationBuilder config) {
+        string environmentName = context.HostingEnvironment.EnvironmentName;
+        AppSettings.Configure.ConfigureEnvironment(config, environmentName);
+    }
+
+    internal static void Configure(HostBuilderContext context, IServiceCollection services) {
+        // Default Activation Handler
+        services.AddTransient<ActivationHandler<LaunchActivatedEventArgs>, DefaultActivationHandler>();
+
+        // Other Activation Handlers
+
+        // Core Services
+        string connectionString = context.Configuration.GetConnectionString("DefaultConnection")!;
+        Core.Configure.ConfigureServices(services, connectionString);
+
+        // Services
+        services.AddSingleton<ILocalSettingsService, LocalSettingsService>();
+        services.AddSingleton<IThemeSelectorService, ThemeSelectorService>();
+        services.AddTransient<INavigationViewService, NavigationViewService>();
+
+        services.AddSingleton<IActivationService, ActivationService>();
+        services.AddSingleton<IPageService, PageService>();
+        services.AddSingleton<INavigationService, NavigationService>();
+
+        // Views and ViewModels
+        services.AddTransient<SettingsViewModel>();
+        services.AddTransient<SettingsPage>();
+        services.AddTransient<HomeViewModel>();
+        services.AddTransient<HomePage>();
+        services.AddTransient<IngredientsViewModel>();
+        services.AddTransient<IngredientsPage>();
+        services.AddTransient<ShellViewModel>();
+        services.AddTransient<ShellPage>();
+
+        // Configuration
+        services.Configure<LocalSettingsOptions>(context.Configuration.GetSection(nameof(LocalSettingsOptions)));
+    }
+}
