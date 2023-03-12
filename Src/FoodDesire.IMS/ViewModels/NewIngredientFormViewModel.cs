76318ab7bdfac217136cdc3c6dce76f7f287fde8
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-03-13 00:30:51 +0530

    IMS: Created viewmodel for NewIngredientForm

--- Src/FoodDesire.IMS/ViewModels/NewIngredientFormViewModel.cs
+++ Src/FoodDesire.IMS/ViewModels/NewIngredientFormViewModel.cs
@@ -2,11 +2,23 @@
 public partial class NewIngredientFormViewModel : ObservableObject {
     private readonly IIngredientsPageService _ingredientsPageService;
     [ObservableProperty]
+    [NotifyPropertyChangedFor(nameof(CanBeCreated))]
     private Ingredient? ingredient;
+    public bool CanBeCreated {
+        get {
+            // Add your validation logic here
+            if (string.IsNullOrEmpty(Ingredient.Name)) return false;
+            if (string.IsNullOrEmpty(Ingredient.Description)) return false;
+            if (Ingredient.MaximumQuantity <= 0) return false;
 
+            // If all validation checks pass, return true
+            return true;
+        }
+    }
     public NewIngredientFormViewModel(IIngredientsPageService ingredientsPageService) {
         _ingredientsPageService = ingredientsPageService;
     }
+
 
     public async Task<Ingredient> CreateIngredient() {
         Ingredient newIngredient = await _ingredientsPageService.AddIngredient(Ingredient!);
--- Src/FoodDesire.IMS/Views/IngredientsPage.xaml.cs
+++ Src/FoodDesire.IMS/Views/IngredientsPage.xaml.cs
@@ -14,23 +14,44 @@ public sealed partial class IngredientsPage : Page {
 
     private async void IngredientList_ItemClick(object sender, ItemClickEventArgs e) {
         IngredientDetails ingredient = (IngredientDetails)e.ClickedItem;
-        ContentDialog dialog = new ContentDialog();
-        dialog.XamlRoot = XamlRoot;
-        dialog.Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style;
-        dialog.Title = $"Request For Supply: {ingredient.Name}";
-        dialog.PrimaryButtonText = "Request";
-        dialog.CloseButtonText = "Cancel";
-        dialog.DefaultButton = ContentDialogButton.Primary;
-
-        dialog.Content = new ViewIngredientItemControl(ingredient);
+        ContentDialog dialog = new ContentDialog {
+            XamlRoot = XamlRoot,
+            Style = (Style)Application.Current.Resources["DefaultContentDialogStyle"],
+            Title = $"Request For Supply: {ingredient.Name}",
+            PrimaryButtonText = "Request",
+            CloseButtonText = "Cancel",
+            DefaultButton = ContentDialogButton.Primary,
+            Content = new ViewIngredientItemControl(ingredient),
+        };
         dialog.DataContext = ((ViewIngredientItemControl)dialog.Content).ViewModel;
-        Binding primaryButtonBinding = new Binding();
-        primaryButtonBinding.Path = new PropertyPath(nameof(ViewIngredientItemViewModel.IsRequested));
-        primaryButtonBinding.Mode = BindingMode.OneWay;
-        primaryButtonBinding.Source = dialog.DataContext;
+        Binding primaryButtonBinding = new Binding {
+            Path = new PropertyPath(nameof(ViewIngredientItemViewModel.IsRequested)),
+            Mode = BindingMode.OneWay,
+            Source = dialog.DataContext,
+        };
         dialog.SetBinding(ContentDialog.IsPrimaryButtonEnabledProperty, primaryButtonBinding);
 
         var result = await dialog.ShowAsync();
+    }
 
+    private async void NewIngredient_Click(object sender, RoutedEventArgs e) {
+        ContentDialog dialog = new ContentDialog {
+            XamlRoot = XamlRoot,
+            Style = (Style)Application.Current.Resources["DefaultContentDialogStyle"],
+            Title = "New Ingredient",
+            PrimaryButtonText = "Create",
+            CloseButtonText = "Cancel",
+            DefaultButton = ContentDialogButton.Primary,
+            Content = new NewIngredientFormControl(),
+        };
+        dialog.DataContext = ((NewIngredientFormControl)dialog.Content).ViewModel;
+        Binding primaryButtonBinding = new Binding {
+            Path = new PropertyPath(nameof(NewIngredientFormViewModel.CanBeCreated)),
+            Mode = BindingMode.OneWay,
+            Source = dialog.DataContext,
+        };
+        dialog.SetBinding(ContentDialog.IsPrimaryButtonEnabledProperty, primaryButtonBinding);
+
+        var result = await dialog.ShowAsync();
     }
 }
