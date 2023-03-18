87ef4985f5505e010fc6f61e68b9a948cb7dfdf3
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-03-18 15:34:09 +0530

    IMS: Added Roles in Account entity

--- Src/FoodDesire.Models/Account.cs
+++ Src/FoodDesire.Models/Account.cs
@@ -8,6 +8,21 @@ public sealed class Account : Entity {
     [MaxLength(15, ErrorMessage = "Password must be 15 characters or less")]
     public required string Password { get; set; }
     public string? VerifyCode { get; set; }
+    [Required]
+    public Role Role { get; set; }
 
     public User? User { get; set; }
+}
+
+public enum Role {
+    [Display(Name = "Admin")]
+    Admin,
+    [Display(Name = "Chef")]
+    Chef,
+    [Display(Name = "Supplier")]
+    Supplier,
+    [Display(Name = "Deliverer")]
+    Deliverer,
+    [Display(Name = "Customer")]
+    Customer
 }
