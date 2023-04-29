61a215adb51b4213ed4a64c94fff42b2164d5eeb
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-04-29 13:20:17 +0530

    WEB: Optimized Nugets

--- Src/FoodDesire.Web.Client/Program.cs
+++ Src/FoodDesire.Web.Client/Program.cs
@@ -3,7 +3,6 @@ using FoodDesire.Web.Client.Helpers;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
-using Syncfusion.Blazor;
 
 WebAssemblyHostBuilder? builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -11,7 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddAuthorizationCore();
 AppConfigurator.ConfigureServices(builder.Services);
-builder.Services.AddSyncfusionBlazor();
 builder.Services.AddMudServices();
 //builder.Services.Configure<PopoverOptions>(options => {
 //    options.ThrowOnDuplicateProvider = false;
