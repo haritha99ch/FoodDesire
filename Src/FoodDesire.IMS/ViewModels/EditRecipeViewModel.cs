30847ade46527a954b47e4648b0e17fbec21cffe
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-05-07 10:10:45 +0530

    IMS: Fixed async warning in EditRecipeViewModel

--- Src/FoodDesire.IMS/ViewModels/EditRecipeViewModel.cs
+++ Src/FoodDesire.IMS/ViewModels/EditRecipeViewModel.cs
@@ -1,20 +1,15 @@
 ﻿namespace FoodDesire.IMS.ViewModels;
 public partial class EditRecipeViewModel : ObservableRecipient, INavigationAware {
-    private readonly IRecipesPageService _recipesPageService;
-
     [ObservableProperty]
     private RecipeForm? _recipeForm;
 
-    public EditRecipeViewModel(IRecipesPageService recipesPageService) {
-        _recipesPageService = recipesPageService;
-    }
+    public EditRecipeViewModel() { }
 
-    public async void OnNavigatedTo(object parameter) {
-        if (parameter is Recipe recipe) {
-            RecipeForm recipeForm = App.GetService<IMapper>().Map<RecipeForm>(recipe);
-            recipeForm.SelectedRecipeCategory = recipe.RecipeCategory;
-            RecipeForm = recipeForm;
-        }
+    public void OnNavigatedTo(object parameter) {
+        if (parameter is not Recipe recipe) return;
+        RecipeForm recipeForm = App.GetService<IMapper>().Map<RecipeForm>(recipe);
+        recipeForm.SelectedRecipeCategory = recipe.RecipeCategory;
+        RecipeForm = recipeForm;
     }
 
     public void OnNavigatedFrom() { }
